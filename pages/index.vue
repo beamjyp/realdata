216e1d80345b97ede8454eaae787241bfb6025d6
beamjyp <beamking555@gmail.com>
2020-10-09 17:24:55 +0700

    commit on master

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
           rel="noopener noreferrer"
           class="button--green"
         >
-          Documentation
+          Do
         </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
